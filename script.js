b8e3fe9ca61a108f441159fb4adeac3ee3e45d66
Optimus Prime <118745157+alunocleverton@users.noreply.github.com>
2024-07-25 17:02:28 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+const caixaPrincipal = document.querySelector(".caixa-principal");
+const caixaPergunta = document.querySelector(".caixa-pergunta");
+const caixaAlternativas = document.querySelector("caixa-alternativas");
+const caixaResultado = document.querySelector(".caixa-resultado");
+const textoResultado = document.querySelector(".texto-resultado");
+const perguntas [
+    {
+        enunciado:"Qual é uma das principais causas do aquecimento global?",
+        alternativa:[
+            "Redução das temperaturas oceânicas"
+            "Emissão de gases de efeito estufa"
+        ]
+    }
+]
