ecd194841fc7eee676e18c5c8cefc05c122e053a
Optimus Prime <118745157+alunocleverton@users.noreply.github.com>
2024-08-01 17:10:23 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -4,11 +4,36 @@ const caixaAlternativas = document.querySelector("caixa-alternativas");
 const caixaResultado = document.querySelector(".caixa-resultado");
 const textoResultado = document.querySelector(".texto-resultado");
 const perguntas [
+    
     {
         enunciado:"Qual é uma das principais causas do aquecimento global?",
         alternativa:[
             "Redução das temperaturas oceânicas"
             "Emissão de gases de efeito estufa"
+        ]
+    }
+
+    {
+        enunciado:"Qual é o principal fator que contribui para o desmatamento das florestas tropicais?",
+        alternativa:[
+            "Mudanças climáticas"
+            "Urbanização"
+        ]
+    }
+
+    {
+        enunciado:"Qual das seguintes ações contribui para a redução da pegada de carbono de uma pessoa?",
+        alternativa:[
+            "Uso de energia renovável"
+            "Consumo de carne vermelha"
+        ]
+    }
+
+    {
+        enunciado:"Qual é o principal impacto ambiental da exploração excessiva de recursos hídricos subterrâneos?",
+        alternativa:[
+            "Escassez de água potável"
+            "Desertificação"
         ]
     }
 ]
